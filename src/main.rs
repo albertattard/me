c71236a6fb82aaadd623c64bdc7ff219416c73bb
Albert Attard <albertattard@gmail.com>
2024-02-22 20:28:41 +0100

    Run markdown

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
+use std::fs::read_to_string;
+
 use crate::cla::Args;
-use std::io;
-use std::path::Path;
+use crate::command::Commands;
+use crate::shell::ShellScript;
 
 mod cla;
 mod command;
+mod shell;
 
 fn main() {
     let args = Args::create();
-    println!("Executing {:?}", args.file_path());
-}
 
-fn read_file(file_path: &Path) -> io::Result<String> {
-    std::fs::read_to_string(file_path)
-}
+    let content = read_to_string(args.file_path()).expect("failed to read MARKDOWN file");
+    let commands = Commands::parse(&content).expect("failed to parse MARKDOWN file");
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn read_file_that_exists() {
-        let file_path = Path::new("target/fixtures/README.md");
-        std::fs::create_dir_all(file_path.parent().unwrap()).unwrap();
-        std::fs::write(file_path, "# README\n\nThis is a README file\n").unwrap();
-
-        let content = read_file(file_path).unwrap();
-        assert_eq!(content, "# README\n\nThis is a README file\n");
-    }
+    ShellScript::new(commands.as_shell_script()).run();
 }
